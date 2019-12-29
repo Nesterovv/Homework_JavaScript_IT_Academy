4efe9b83fa7f6a1f2f113544ba06839a9b4e15c4
Anton Nesterov <antonnesterov939@gmail.com>
2019-12-29 22:59:40 +0300

    edf

--- TREESUM.js
+++
@@ -1,9 +0,0 @@
-let a = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], 
-    [ 9, [] ], 
-    1, 8];
-
-function treeSum(){
-    let result = a.flat(Infinity);
-    return result.reduce((sum, current) => sum + current, 0);
-};
-console.log (treeSum());
